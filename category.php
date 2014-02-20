82d30df3d281296d5840facc2284bbbf78ceabd8
Stuart Turner <stuart@turner-isageek.com>
2014-02-20 17:44:54 -0600

    Fix CSS on category page

--- category.php
+++ category.php
@@ -12,13 +12,14 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ): ?>
-<h2>Category Archive: <?php echo single_cat_title( '', false ); ?></h2>
+<div id="category">
+<h1><?php echo single_cat_title( '', false ); ?></h1>
 <ol>
 <?php while ( have_posts() ) : the_post(); ?>
 	<li>
 		<article>
 			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
 			<?php the_content(); ?>
 		</article>
 	</li>
@@ -27,5 +28,6 @@
 <?php else: ?>
 <h2>No posts to display in <?php echo single_cat_title( '', false ); ?></h2>
 <?php endif; ?>
+</div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
