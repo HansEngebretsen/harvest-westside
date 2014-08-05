40576b30504b3232433594b193a6ef9033b65fc5
Stuart Turner <stuart@turner-isageek.com>
2014-08-05 17:04:42 -0500

    Shrink video; add two new images to clinic page

--- page-clinic.php
+++ page-clinic.php
@@ -12,8 +12,10 @@
  <div class="main-content clinic-page">
  	<h1 class="title"> <img src="<?php bloginfo('template_directory'); ?>/img/Template/harvest-clinic-logo.png" class="clinic-logo" alt="The Harvest Westside Clinic"></h1>
  	<p class="clinic-description"><?php the_field('clinic_main_description'); ?></p>
-   <div class='embed-container'>
-     <iframe src='https://player.vimeo.com/video/87631611' frameborder='0' webkitAllowFullScreen='' mozallowfullscreen='' allowFullScreen=''></iframe>
+   <div class="right-section">
+     <div class='embed-container'>
+       <iframe src='https://player.vimeo.com/video/87631611' frameborder='0' webkitAllowFullScreen='' mozallowfullscreen='' allowFullScreen=''></iframe>
+     </div>
    </div>
    <p></p>
  		<img src="<?php the_field('clinic_main_image'); ?>" class="clinic-photo" alt="The Harvest Westside Clinic">
@@ -29,7 +31,10 @@
         </section>
       </div>
     </aside>
-	
+  
+ 		<img src="<?php the_field('image_2'); ?>" alt="The Harvest Westside Clinic">
+    <br />
+ 		<img src="<?php the_field('image_3'); ?>" alt="The Harvest Westside Clinic">
 </div>
 <?php endwhile; ?>
 
